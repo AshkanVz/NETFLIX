39f388711f19e63dea1b71618f5430c748f25756
Ashkan Valizadeh <ashvalizadeh@gmail.com>
2022-09-01 17:48:29 +0430

    new change on toast

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,44 @@ import Main from "../components/Main";
 import Navbar from "../components/Navbar";
 import Row from "../components/Row";
 import requests from "../Requests";
+import { UserAuth } from "../context/AuthContext";
+import classNames from "classnames";
+import toast, { Toaster } from "react-hot-toast";
+import { MdOutlineClose } from "react-icons/md";
+import { HiLightningBolt } from "react-icons/hi";
+import styles from "./Login.module.css";
+
 
 const Home = () => {
+  const {user} = UserAuth();
+  
+  {user && toast.custom(
+    (t) => (
+      <div
+        className={classNames([
+          styles.successnotif,
+          t.visible ? "top-0" : "-top-96",
+        ])}
+      >
+        <div className={styles.iconWrapper}>
+          <HiLightningBolt />
+        </div>
+        <div className={styles.contentWrapper}>
+          <h1>Seuccess</h1>
+          <p>
+            Signin successfully !!
+          </p>
+        </div>
+        <div className={styles.closeIcon} onClick={() => toast.dismiss(t.id)}>
+          <MdOutlineClose />
+        </div>
+      </div>
+    ),
+    { id: "unique-notification", position: "top-center" }
+  ); }
   return (
     <div className="bg-black">
-     
+    <Toaster/>
       <Main />
       <Row rowID="1" title="UpComing" fetchURL={requests.requestUpcoming} />
       <Row rowID="2" title="Popular" fetchURL={requests.requestPopular} />
